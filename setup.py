afc3f2a79baf5ccce5e8f9490a5b170c5a58af96
gwy15 <gwy15thu@gmail.com>
2019-04-14 17:38:20 +0800

    Create CI, license, setup.py. Ignore venv and egg in .gitignore.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="async_pool",
+    version="0.1",
+    keywords=("async", "multiprocessing", "producer-consumer"),
+    description="A simple wrapper of library asyncio providing a multiprocessing-library-like coroutine worker pool.",
+    license="MIT Licence",
+
+    url="https://github.com/gwy15/async_pool",
+    author="gwy15",
+    author_email="gwy15thu@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any"
+)
